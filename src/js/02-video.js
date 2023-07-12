989d73efbc0daef7f2180685d700353212f2a0f3
Mastardont <107326331+Mastardont@users.noreply.github.com>
2023-07-12 22:19:50 +0300

    goit-js-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,12 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const STORAGE_KEY = 'videoplayer-current-time';
+
+playSaved();
 
 player.on('play', function () {
   console.log('played the video!');
@@ -13,18 +16,22 @@ player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
 
-player.on('timeupdate', function (data) {
-  // Зберігаємо час відтворення в локальне сховище
-  localStorage.setItem(STORAGE_KEY, data.seconds);
-});
+player.on(
+  'timeupdate',
+  // застосовуємо throttle
+  throttle(function (data) {
+    // Зберігаємо час відтворення в локальне сховище
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+  }, 1000)
+);
 
-player.on('play', function () {
+function playSaved() {
   // Перевіряємо наявність збереженого часу відтворення
   if (localStorage.getItem(STORAGE_KEY)) {
     // Використовуємо збережений час відтворення.
     player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
   }
-});
+}
 
 player.on('ended', function () {
   // Видаляємо збережений час відтворення з локального сховища по закінченню відео
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,7 +4,7 @@ const form = document.querySelector('.feedback-form');
 
 const STORAGE_KEY = 'feedback-form-state';
 
-const formData = {};
+let formData = {};
 
 // застосовуємо throttle
 form.addEventListener('input', throttle(onFormInput, 500));
@@ -15,6 +15,18 @@ formOutput();
 // Зберігаємо текс у сховище
 function onFormInput(evt) {
   formData[evt.target.name] = evt.target.value;
+
+  if (form[0].value) {
+    formData[form[0].name] = form[0].value;
+  } else {
+    formData[form[0].name] = ' ';
+  }
+
+  if (form[1].value) {
+    formData[form[1].name] = form[1].value;
+  } else {
+    formData[form[1].name] = ' ';
+  }
 
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
@@ -27,6 +39,18 @@ function formOutput() {
     form[0].value = saveFormValue['email'];
     form[1].value = saveFormValue['message'];
   }
+
+  if (form[0].value) {
+    formData[form[0].name] = form[0].value;
+  } else {
+    formData[form[0].name] = ' ';
+  }
+  
+  if (form[1].value) {
+    formData[form[1].name] = form[1].value;
+  } else {
+    formData[form[1].name] = ' ';
+  }
 }
 
 // Налаштування кнопки submit
@@ -36,7 +60,7 @@ function onFormFeedback(evt) {
   evt.preventDefault();
 
   // виводимо ємність у консоль
-  console.log(localStorage.getItem(STORAGE_KEY));
+  console.log(formData);
 
   // очищення сховища та форми
   evt.currentTarget.reset();
